61b44a30ae7227feb86a7c8e20ebffc9f49c2e0f
Molly McCarron <mccarronmollye@gmail.com>
2019-02-20 16:33:21 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+
+    if session[:name] == nil || session[:name] == ''
+      redirect_to '/login'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/login'
+  end
+
+end
